eb0c5708122d56485cfea620a24f3d1567d6ab8e
David Girsvaldas <dgi@scadaminds.com>
2020-01-26 13:26:53 +0100

    refactor: combinations.find method

--- combination_finder.py
+++ combination_finder.py
@@ -4,31 +4,8 @@ import combinations
 
 
 def find(cards: List[Card]):
-    combo = combinations.find_royal_flush(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_straight_flush(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_four_of_a_kind(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_full_house(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_flush(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_straight(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_three_of_a_kind(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_two_pairs(cards)
-    if combo is not None:
-        return combo
-    combo = combinations.find_pair(cards)
-    if combo is not None:
-        return combo
-    return combinations.find_high_card(cards)
+    return combinations.find_royal_flush(cards) or combinations.find_straight_flush(
+        cards) or combinations.find_four_of_a_kind(cards) or combinations.find_full_house(
+        cards) or combinations.find_flush(cards) or combinations.find_straight(
+        cards) or combinations.find_three_of_a_kind(cards) or combinations.find_two_pairs(
+        cards) or combinations.find_pair(cards) or combinations.find_high_card(cards)
