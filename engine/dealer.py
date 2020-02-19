8b893107182eb85f1597df97fca3969b8208dff8
David Girsvaldas <david.girsvaldas@gmail.com>
2020-02-19 20:49:23 +0100

    refactor: recursive loop for playing flop

--- engine/dealer.py
+++ engine/dealer.py
@@ -77,9 +77,12 @@ class Dealer:
         return round_of_calls_to_make(bb_player, True, amount_to_match)
 
     def play_flop(self):
-        def ask_players_for_actions(player_who_raised, new_raised_amount):
+        def ask_players_for_actions(player_who_raised, new_raised_amount, include_last_player):
             next_player = self.seating.next_player_after_player(player_who_raised)
-            for i in range(len(self.seating.players)-1):
+            amount_of_calls_to_make = len(self.seating.players)
+            if not include_last_player:
+                amount_of_calls_to_make -= 1
+            for i in range(amount_of_calls_to_make):
                 p_action, p_amount = next_player.act(new_raised_amount)
                 if p_action == Action.ACTION_FOLD:
                     self.player_folds(next_player)
@@ -87,24 +90,14 @@ class Dealer:
                     self.player_calls(next_player, p_amount)
                 elif p_action == Action.ACTION_RAISE:
                     self.player_calls(next_player, p_amount)
-                    return ask_players_for_actions(next_player, p_amount)
+                    return ask_players_for_actions(next_player, p_amount, False)
                 if self.is_winner_determined():
                     return self.award_winner()
                 next_player = self.seating.next_player_after_player(next_player)
 
         self.add_community_cards(1)  # todo should be revealed at the end, not start
         last_player_to_go = self.seating.players[0]  # todo remove assumption that button sits at position 0
-        player = self.seating.next_player_after_player(last_player_to_go)
-        for i in range(len(self.pot.players)):
-            action, amount = player.act(0)
-            if action == Action.ACTION_FOLD:
-                self.player_folds(player)
-            if action == Action.ACTION_RAISE:
-                self.player_calls(player, amount)
-                return ask_players_for_actions(player, amount)
-            if self.is_winner_determined():
-                return self.award_winner()
-            player = self.seating.next_player_after_player(player)
+        return ask_players_for_actions(last_player_to_go, 10, True) # todo remove assumption that big blind size is always 10
 
     def player_calls(self, player, amount):
         player.stack -= amount - player.money_in_pot
